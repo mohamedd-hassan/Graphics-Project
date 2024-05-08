21b66236f5d04f65e50fe8abf1aaf9215e21c0c4
Mohamed Hassan <127283755+mohamedd-hassan@users.noreply.github.com>
2024-05-08 23:55:07 +0300

    -

--- GraphicsAssignment/src/Pacman/PacMaze.java
+++ GraphicsAssignment/src/Pacman/PacMaze.java
@@ -15,34 +15,43 @@ public class PacMaze extends JPanel {
         g.setColor(new Color(0, 27, 248));
 //        g.fillRect(0,56, 490, 10);
 
+        //1
         g2d.drawRoundRect(42,100,54,35, 15, 15);
         g2d.drawRoundRect(131,100,70,35, 15, 15);
+        //2
         g2d.drawRoundRect(288,100,70,35, 15, 15);
         g2d.drawRoundRect(394,100,54,35, 15, 15);
 
 //        g2d.drawRoundRect(233, 57, 23, 80, 15,10);
 
+        //3
         g2d.drawRoundRect(42,170,54,18,15,15);
         g2d.drawRoundRect(394,170,54,18,15,15);
-
+        //4
         g2d.drawRoundRect(131,326,18,70,15,15);
         g2d.drawRoundRect(340,326,18,70,15,15);
-
+        //5
         g2d.drawRoundRect(131,432,70,19,15,15);
         g2d.drawRoundRect(288,432,70,19,15,15);
 
-
+        //6
         g2d.drawPolygon(new int[]{130, 149, 149, 200, 200, 149, 149, 130}, new int[]{170, 170, 222, 222, 240, 240, 290, 290},8);
         g2d.drawPolygon(new int[]{359,340,340,290,290,340,340,359},new int[]{170, 170, 222, 222, 240, 240, 290, 290},8);
-
-        g2d.drawPolygon(new int[]{},new int[]{},8);
-        g2d.drawPolygon([new int[]{},new int[]{},8);
-//
-//        g2d.drawPolygon([],[],);
-//        g2d.drawPolygon([],[],);
-//
-//        g2d.drawPolygon([],[],8);
-//        g2d.drawPolygon([],[],8);
-//        g2d.drawPolygon([],[],8);
+        //7
+//        g2d.drawPolygon(new int[]{},new int[]{},8);
+//        g2d.drawPolygon([new int[]{},new int[]{},8);
+        //8
+//        g2d.drawPolygon(new int[]{},new int[]{},6);
+//        g2d.drawPolygon(new int[]{},new int[]{},6);
+        //9
+//        g2d.drawPolygon(new int[]{},new int[]{},8);
+//        g2d.drawPolygon(new int[]{},new int[]{},8);
+//        g2d.drawPolygon(new int[]{},new int[]{},8);
+        //Ghost House
+//        g2d.drawPolygon(new int[]{},new int[]{},);
+        //Outer wall Upper Half
+//        g2d.drawPolygon(new int[]{},new int[]{},);
+        //Outer wall Lower half
+//        g2d.drawPolygon(new int[]{},new int[]{},);
     }
 }
